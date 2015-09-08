25d9801d07c4fb9c75869ef9597540798df38e72
rohdimp24 <rohitagarwal24@gmail.com>
2015-09-08 15:00:58 +0530

    this is a common way to handle the exceptions in angular. Whenever the exception occurs we will get the alert

---
+++ app/appConfig.js
@@ -0,0 +1,15 @@
+/**
+ * Created by rohitagarwal on 9/8/15.
+ */
+
+//pretty standard exception handling code
+(function(){
+    angular.module('app').config(function ($provide) {
+        $provide.decorator("$exceptionHandler", ["$delegate", function ($delegate) {
+            return function (exception, cause) {
+                $delegate(exception, cause);
+                alert(exception.message);
+            };
+        }]);
+    });
+}())
