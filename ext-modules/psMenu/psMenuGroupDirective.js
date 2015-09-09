f52eb3a6de1322a3702dfa608ee387206e00a1d5
rohdimp24 <rohitagarwal24@gmail.com>
2015-09-09 17:16:20 +0530

    basic changes to add menu group

---
+++ ext-modules/psMenu/psMenuGroupDirective.js
@@ -0,0 +1,35 @@
+/**
+ * Created by rohitagarwal on 9/9/15.
+ */
+/**
+ * Created by rohitagarwal on 9/8/15.
+ */
+(function(){
+
+    angular.module("psMenu").directive("psMenuGroup",function(){
+
+        return {
+
+            //use the parent controller
+            require:'^psMenu',
+            templateUrl:"ext-modules/psMenu/psMenuGroupTemplate.html",
+            transclude:true,
+            scope:{
+                label:'@',
+                icon:'@',
+
+            },
+            //in the link functon we can also pass a controller object
+            link:function(scope,elem,attr,ctrl){
+
+
+            }
+        }
+
+
+    });
+
+
+
+
+}())
