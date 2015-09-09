5cf23071d92c6fa34b28e24b79c75b0f68a44e4d
rohdimp24 <rohitagarwal24@gmail.com>
2015-09-09 08:30:14 +0530

    added a bar button that is supposed to show up for the menu options on the small device. We did some CSS changes so that the menu button is fixed on th right hand side when the screen size is small

--- ext-modules/psMenu/psMenuItemDirective.js
+++ ext-modules/psMenu/psMenuItemDirective.js
@@ -26,6 +26,8 @@
                     //now we want to use the controller to handle the logic. this is the psmenucontroller of the parent
                     //aslo since we are using teh jquery and want to use the angular controller we need to tell the angular that
                     //the scope might have changed. this we will do using the appy
+                    //I guess we cannot use scope.setActiveElement as this scope is an isoloated scope which means it is not
+                    //same as the scope of the controller that we are passing
                     scope.$apply(function(){
                         //call tyhe controller function
                         ctrl.setActiveElement(elem);
